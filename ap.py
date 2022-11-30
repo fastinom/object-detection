b945974d75edc6f45069eaea9d727afc3280f2a9
fastinom <107068651+fastinom@users.noreply.github.com>
2022-11-30 07:48:40 +0200

    Update ap.py

--- ap.py
+++ ap.py
@@ -6,23 +6,14 @@ import streamlit as st
 from collections import deque
 import os
 import subprocess
-
-
-
 # loading the saved model
 #loaded_model = load_model('model.hd')
-
-
 # Specify the height and width to which each video frame will be resized in our dataset.
 IMAGE_HEIGHT , IMAGE_WIDTH = 224, 224
-
 # Specify the list containing the names of the classes used for training. Feel free to choose any set of classes.
 CLASSES_LIST = ["CricketShot", "PlayingCello", "TennisSwing", "Punch", "ShavingBeard"]  
-
 # Specify the number of frames of a video that will be fed to the model as one sequence.
 SEQUENCE_LENGTH = 20
-
-
 # creating a function for Prediction
 def predict_on_video(video_file_path, output_file_path, SEQUENCE_LENGTH):
     '''
@@ -31,58 +22,43 @@ def predict_on_video(video_file_path, output_file_path, SEQUENCE_LENGTH):
     output_file_path: The path where the ouput video with the predicted action being performed overlayed will be stored.
     SEQUENCE_LENGTH:  The fixed number of frames of a video that can be passed to the model as one sequence.
     '''
-
     # Initialize the VideoCapture object to read from the video file.
     video_reader = cv2.VideoCapture(video_file_path)
-
     # Get the width and height of the video.
     original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
     original_video_height = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
     # Initialize the VideoWriter Object to store the output video in the disk.
     video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc('M', 'P', '4', 'V'), 
                                    video_reader.get(cv2.CAP_PROP_FPS), (original_video_width, original_video_height))
-
     # Declare a queue to store video frames.
     frames_queue = deque(maxlen = SEQUENCE_LENGTH)
-
     # Initialize a variable to store the predicted action being performed in the video.
     predicted_class_name = ''
-
     # Iterate until the video is accessed successfully.
     while video_reader.isOpened():
-
         # Read the frame.
         ok, frame = video_reader.read() 
         
         # Check if frame is not read properly then break the loop.
         if not ok:
             break
-
         # Resize the Frame to fixed Dimensions.
         resized_frame = cv2.resize(frame, (IMAGE_HEIGHT, IMAGE_WIDTH))
         
         # Normalize the resized frame by dividing it with 255 so that each pixel value then lies between 0 and 1.
         normalized_frame = resized_frame / 255
-
         # Appending the pre-processed frame into the frames list.
         frames_queue.append(normalized_frame)
-
         # Check if the number of frames in the queue are equal to the fixed sequence length.
         if len(frames_queue) == SEQUENCE_LENGTH:
-
             # Pass the normalized frames to the model and get the predicted probabilities.
             predicted_labels_probabilities = loaded_model.predict(np.expand_dims(frames_queue, axis=1))[0]
-
             # Get the index of class with highest probability.
             predicted_label = np.argmax(predicted_labels_probabilities)
-
             # Get the class name using the retrieved index.
             predicted_class_name = CLASSES_LIST[predicted_label]
-
         # Write predicted class name on top of the frame.
         cv2.putText(frame, predicted_class_name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
         # Write The frame into the disk using the VideoWriter Object.
         video_writer.write(frame)
         
@@ -90,9 +66,9 @@ def predict_on_video(video_file_path, output_file_path, SEQUENCE_LENGTH):
     video_reader.release()
     video_writer.release()
 
-  
 def main():  
     # giving a title
+    st.title('Video Classification')
     st.title('Object detection')
     #Upload video file
     uploaded_file = st.file_uploader("Choose a video...", type=["mp4", "mpeg", "avi"])
